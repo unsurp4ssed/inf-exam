ecb38bfdc2768e84cd142e82a438334f5c807822
unsurp4ssed <116448397+unsurp4ssed@users.noreply.github.com>
2023-01-20 09:47:20 +0300

    Create app.py

---
+++ task2/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from string import digits
+app = Flask(__name__)
+
+def f(x):
+    s = 0
+    b = ''
+    if all(i in digits for i in x):
+        for j in x: s+=int(j)
+        return s
+    else: 
+        for j in x:
+            if j not in digits: b += j
+        return b
+    
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    result = ''
+    if request.method == "POST": 
+        result = f(str(request.form["input"]))
+    return render_template('index.html', result=result)
+    
+if __name__ == "__main__":
+    app.run()
